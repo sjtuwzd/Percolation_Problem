2940ffef8e03afecf8618280b5ba2a69f3f3b052
WZD <sjtuwzd@Umich.edu>
2018-07-17 15:52:27 +0800

    + statistics

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -6,6 +6,7 @@ public class PercolationStats {
 
     private int numberOfTrials;
     private int length;
+    private double[] results;
 
     /**
      * Perform trials independent experiments on an n-by-n grid
@@ -15,9 +16,22 @@ public class PercolationStats {
     public PercolationStats(int n, int trials){
         this.numberOfTrials = trials;
         this.length = n;
+        this.results = new double[this.numberOfTrials];
+        int gridsTotal = this.length * this.length;
+        int Max = this.length;
+        int Min = 1;
 
         for(int i = 0; i<this.numberOfTrials;i++){
+            Percolation percolation = new Percolation(this.length);
 
+            //loop until percolates
+            while(!percolation.percolates()){
+
+                int row = (int) (Math.round(Math.random() * (Max - Min) + Min));
+                int col = (int) (Math.round(Math.random() * (Max - Min) + Min));
+                percolation.open(row, col);
+            }
+            results[i] = ((double)percolation.numberOfOpenSites()/(double)(gridsTotal));
         }
     }
 
@@ -27,8 +41,8 @@ public class PercolationStats {
      * @return
      */
     public double mean(){
-        return 0;
 
+        return StdStats.mean(results);
     }
 
 
@@ -36,8 +50,8 @@ public class PercolationStats {
      * sample standard deviation of percolation threshold
      * @return
      */
-    public double stddev(){
-        return 0;
+    public double stddev() {
+        return StdStats.stddev(results);
     }
 
 
@@ -46,7 +60,7 @@ public class PercolationStats {
      * @return
      */
     public double confidenceLo(){
-        return 0;
+        return (this.mean()-(1.96*this.stddev())/(Math.sqrt((double)this.numberOfTrials)));
     }
 
 
@@ -55,7 +69,7 @@ public class PercolationStats {
      * @return
      */
     public double confidenceHi(){
-        return 0;
+        return (this.mean()+(1.96*this.stddev())/(Math.sqrt((double)this.numberOfTrials)));
     }
 
 
@@ -64,6 +78,12 @@ public class PercolationStats {
      * @param args
      */
     public static void main(String[] args){
+        PercolationStats percolationStats = new PercolationStats(200, 100);
+        System.out.println(percolationStats.results[0]);
+        System.out.println(percolationStats.mean());
+        System.out.println(percolationStats.stddev());
+        System.out.println(percolationStats.confidenceLo());
+        System.out.println(percolationStats.confidenceHi());
 
     }
 }
